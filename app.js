3c7742b8e8cf41df3a6845113c2e0b40c269c83c
Adam Bertus <bertus.rocky@gmail.com>
2019-06-26 08:50:15 +0100

    completed project

---
+++ app.js
@@ -0,0 +1,32 @@
+//init github
+const github = new Github;
+
+//init ui
+const ui = new UI;
+
+//search input
+
+const searchUser = document.getElementById('searchUser');
+
+// search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  //get text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+    // make http call
+    github.getUser (userText)
+      .then(data => {
+        if(data.profile.message === 'Not Found') {
+          //show alert
+          ui.showAlert('User not found', 'alert alert-danger');
+        } else {
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      })
+  } else {
+    //clear profile
+    ui.clearProfile();
+  }
+});
